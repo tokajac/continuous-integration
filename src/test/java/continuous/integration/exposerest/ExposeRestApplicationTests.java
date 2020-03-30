6eebe916dbc40a10aa6dc66b879fbc67638cd441
Imre Tokai <imre.tokai@gmail.com>
2020-03-30 11:29:56 +0200

    basic failing test case

--- src/test/java/continuous/integration/exposerest/ExposeRestApplicationTests.java
+++ src/test/java/continuous/integration/exposerest/ExposeRestApplicationTests.java
@@ -1,18 +1,49 @@
 package continuous.integration.exposerest;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.testng.annotations.BeforeClass;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
-class ExposeRestApplicationTests {
+class ExposeRestApplicationTests extends AbstractTestNGSpringContextTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	public void testHandling(){
-
+	@BeforeClass
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
 
+	@Test
+	public void testHandling() throws Exception {
+		String postBodyString = "{\"email\":\"test@test.com\",\"couponCode\":\"12football\",\"firstName\":\"first\",\"lastName\":\"last\",\"countryName\":\"Hungary\",\"age\":38,\"address\":\"SomeCity zipCode SomeStreet houseNo\"}";
+
+		mockMvc.perform( post( "/receiveCoupon" ).content( postBodyString ).contentType(MediaType.APPLICATION_JSON) ).andExpect( status().isOk() );
+	}
+
+	@Test
+	public void testBasicFailure() throws Exception {
+		String postBodyString = "{\"email\":\"test@test.com\",\"couponCode\":\"12football\"}";
+
+		mockMvc.perform( post( "/receiveCoupon" ).content( postBodyString ).contentType(MediaType.APPLICATION_JSON) )
+				.andExpect( status().isOk() )
+				.andExpect( jsonPath("$.message").value("wrong") );
+	}
 }
